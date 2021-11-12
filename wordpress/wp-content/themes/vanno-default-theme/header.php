8c3dd053cc114de6ff37e339b2b0a85b37ad75df
andres613 <andres613@misena.edu.co>
2021-11-11 21:36:45 -0500

    Create vanno's default theme base

---
+++ wordpress/wp-content/themes/vanno-default-theme/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="VANNO - Premium directory consumer reviews and listings template by Ansonika">
+        <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico" type="image/x-icon">
+        <title>Vanno - Consumers Reviews and Rating Directory Preview - ThemeForest</title>
+    </head>
+
+    <body>
+
+        <header class="header">
+            <h1>header.php</h1>
+        </header>
